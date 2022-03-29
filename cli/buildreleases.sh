6ff3574906ff27632968ce5c5830a6521b3a2521
MattyJ007 <matthew.dickie@jembi.org>
2022-03-29 13:38:24 +0200

    Make the binary name generic
    Starter kit can be reused by other projects
    
    PLAT-168

--- cli/buildreleases.sh
+++ cli/buildreleases.sh
@@ -1,5 +1,5 @@
 mkdir -p bin
-GOOS=darwin GOARCH=amd64 go build && mv ./cli ./bin/goinstant-macos \
-&& GOOS=linux GOARCH=amd64 go build && mv ./cli ./bin/goinstant-linux \
-&& GOOS=windows GOARCH=amd64 go build && mv ./cli.exe ./bin/goinstant.exe\
+GOOS=darwin GOARCH=amd64 go build && mv ./cli ./bin/gocli-macos \
+&& GOOS=linux GOARCH=amd64 go build && mv ./cli ./bin/gocli-linux \
+&& GOOS=windows GOARCH=amd64 go build && mv ./cli.exe ./bin/gocli.exe\
 && go clean
