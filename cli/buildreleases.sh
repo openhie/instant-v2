3e2ba299df6324691eae3264225d3a5ece1b7859
MattyJ007 <matthew.dickie@jembi.org>
2022-03-29 13:14:54 +0200

    Remove references to temp dir /goinstant
    Binaries will be temporarily placed into the existing cli dir before
    being moved to the bin dir
    
    PLAT-168

--- cli/buildreleases.sh
+++ cli/buildreleases.sh
@@ -1,5 +1,5 @@
 mkdir -p bin
-GOOS=darwin GOARCH=amd64 go build && mv ./goinstant ./bin/goinstant-macos \
-&& GOOS=linux GOARCH=amd64 go build && mv ./goinstant ./bin/goinstant-linux \
-&& GOOS=windows GOARCH=amd64 go build && mv ./goinstant.exe ./bin/goinstant.exe \
+GOOS=darwin GOARCH=amd64 go build && mv ./cli ./bin/goinstant-macos \
+&& GOOS=linux GOARCH=amd64 go build && mv ./cli ./bin/goinstant-linux \
+&& GOOS=windows GOARCH=amd64 go build && mv ./cli.exe ./bin/goinstant.exe\
 && go clean
