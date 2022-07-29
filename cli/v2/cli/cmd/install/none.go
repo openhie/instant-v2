ebac21888aad54960d8203f78759c2ce336bb3f1
Castello Govender <castello.govender@jembi.org>
2022-07-29 13:04:46 +0200

    Added install commands to replicate v1 install command

--- cli/v2/cli/cmd/install/none.go
+++ cli/v2/cli/cmd/install/none.go
@@ -2,6 +2,7 @@ package install
 
 import (
 	v1 "github.com/openhie/package-starter-kit/cli"
+	"github.com/openhie/package-starter-kit/cli/v2/cli/util"
 	"github.com/spf13/cobra"
 )
 
@@ -12,8 +13,21 @@ func InitNoneCommand() *cobra.Command {
 		Run: func(cmd *cobra.Command, args []string) {
 			params := &v1.Params{}
 			params.TypeAuth = "None"
+
+			urlEntry, err := cmd.Flags().GetString("url-entry")
+			util.LogError(err)
+			fhirServer, err := cmd.Flags().GetString("fhir-server")
+			util.LogError(err)
+
+			err = v1.LoadIGpackage(urlEntry, fhirServer, params)
+			util.LogError(err)
 		},
 	}
+
+	flags := cmd.Flags()
+
+	flags.StringP("url-entry", "url", "", "The url entry for the fhir IG")
+	flags.StringP("fhir-server", "fhir", "", "The fhir server for the IG")
 
 	return cmd
 }
