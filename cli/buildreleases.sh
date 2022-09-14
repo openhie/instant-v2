bfefec1625a3da53f53c0dea0a8ede76bfa9555b
mark-labuschagne <marklabuschagne7@gmail.com>
2022-09-14 17:08:42 +0200

    buildreleases.sh to match new folder structure

--- cli/buildreleases.sh
+++ cli/buildreleases.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # This script builds Linux, Windows and MacOS binaries into the cli/bin directory
 mkdir -p bin
-GOOS=darwin GOARCH=amd64 go build && mv ./cli ./bin/gocli-macos &&
-    GOOS=linux GOARCH=amd64 go build && mv ./cli ./bin/gocli-linux &&
-    GOOS=windows GOARCH=amd64 go build && mv ./cli.exe ./bin/gocli.exe && go clean
+
+cd cmd
+
+GOOS=darwin GOARCH=amd64 go build -o ../bin/gocli-macos
+GOOS=linux GOARCH=amd64 go build -o ../bin/gocli-linux
+GOOS=windows GOARCH=amd64 go build -o ../bin/gocli.exe
+go clean
