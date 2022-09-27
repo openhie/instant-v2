112edde44d686f14dcb5aa618b1a30e54161df69
Castello Govender <castello.govender@jembi.org>
2022-09-27 15:33:08 +0200

    Updated code comments

--- v2/cli/util/util.go
+++ v2/cli/util/util.go
@@ -104,13 +104,13 @@ func CloneRepo(url, dest, sshKeyPath, sshPassword string) error {
 }
 
 func unzipFile(f *zip.File, destination string) error {
-	// 4. Check if file paths are not vulnerable to Zip Slip
+	// Check if file paths are not vulnerable to Zip Slip
 	filePath := filepath.Join(destination, f.Name)
 	if !strings.HasPrefix(filePath, filepath.Clean(destination)+string(os.PathSeparator)) {
 		return fmt.Errorf("invalid file path: %s", filePath)
 	}
 
-	// 5. Create directory tree
+	// Create directory tree
 	if f.FileInfo().IsDir() {
 		if err := os.MkdirAll(filePath, os.ModePerm); err != nil {
 			return err
@@ -122,14 +122,14 @@ func unzipFile(f *zip.File, destination string) error {
 		return err
 	}
 
-	// 6. Create a destination file for unzipped content
+	// Create a destination file for unzipped content
 	destinationFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
 	if err != nil {
 		return err
 	}
 	defer destinationFile.Close()
 
-	// 7. Unzip the content of a file and copy it to the destination file
+	// Unzip the content of a file and copy it to the destination file
 	zippedFile, err := f.Open()
 	if err != nil {
 		return err
@@ -143,20 +143,20 @@ func unzipFile(f *zip.File, destination string) error {
 }
 
 func UnzipSource(source, destination string) error {
-	// 1. Open the zip file
+	// Open the zip file
 	reader, err := zip.OpenReader(source)
 	if err != nil {
 		return err
 	}
 	defer reader.Close()
 
-	// 2. Get the absolute destination path
+	// Get the absolute destination path
 	destination, err = filepath.Abs(destination)
 	if err != nil {
 		return err
 	}
 
-	// 3. Iterate over zip files inside the archive and unzip each of them
+	// Iterate over zip files inside the archive and unzip each of them
 	for _, f := range reader.File {
 		err := unzipFile(f, destination)
 		if err != nil {
