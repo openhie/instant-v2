a21a5246ff0102c264cbb56ba25bf4eb1d831c92
mark-labuschagne <marklabuschagne7@gmail.com>
2022-10-04 10:28:43 +0200

    Removed cli-v1

--- cli/buildreleases.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script builds Linux, Windows and MacOS binaries into the cli/bin directory
-mkdir -p bin
-GOOS=darwin GOARCH=amd64 go build && mv ./cli ./bin/gocli-macos &&
-    GOOS=linux GOARCH=amd64 go build && mv ./cli ./bin/gocli-linux &&
-    GOOS=windows GOARCH=amd64 go build && mv ./cli.exe ./bin/gocli.exe && go clean
