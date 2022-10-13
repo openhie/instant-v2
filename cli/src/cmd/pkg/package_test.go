15b5694b1f471bd69a42c6e51d8caf1f5e9356e9
mark-labuschagne <marklabuschagne7@gmail.com>
2022-10-13 16:28:06 +0200

    refactored test structure

--- cli/src/cmd/pkg/package_test.go
+++ cli/src/cmd/pkg/package_test.go
@@ -12,6 +12,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var configCaseOne = core.Config{
+	Image:    "jembi/go-cli-test-image",
+	LogPath:  "/tmp/logs",
+	Packages: []string{"client", "dashboard-visualiser-jsreport"},
+	CustomPackages: []core.CustomPackage{
+		{
+			Id:          "disi-on-platform",
+			Path:        "git@github.com:jembi/disi-on-platform.git",
+			SshKey:      "./id_rsa",
+			SshPassword: "./id_rsa_password.txt",
+		},
+	},
+	Profiles: []core.Profile{
+		{
+			Name:     "dev",
+			EnvFiles: []string{"../test-conf/.env.test"},
+			Packages: []string{"dashboard-visualiser-jsreport", "disi-on-platform"},
+			Dev:      true,
+		},
+	},
+}
+
 func Test_unmarshalConfig(t *testing.T) {
 	wd, err := os.Getwd()
 	jtest.RequireNil(t, err)
@@ -31,18 +53,40 @@ func Test_loadInProfileParams(t *testing.T) {
 	wd, err := os.Getwd()
 	jtest.RequireNil(t, err)
 
-	// case: return error from conflicting command-line --dev flag and dev: false config.yml profile flag
-	cmd, config := setupLoadInProfileParams(t, wd+"/../../features/unit-test-configs/config-case-2.yml")
+	type cases struct {
+		profileName         string
+		boolFlagName        string
+		configFilePath      string
+		expectedErrorString string
+	}
 
-	cmd.Flags().Bool("dev", false, "")
-	err = cmd.Flags().Set("dev", "true")
-	jtest.RequireNil(t, err)
+	testCases := []cases{
+		// case: return error from conflicting command-line --dev flag and 'dev: false' config.yml profile flag
+		{
+			profileName:         "non-dev",
+			boolFlagName:        "dev",
+			configFilePath:      wd + "/../../features/unit-test-configs/config-case-2.yml",
+			expectedErrorString: ErrConflictingDevFlag.Error(),
+		},
+		// case: return error from conflicting command-line --only flag and 'only: false' config.yml profile flag
+		{
+			profileName:         "non-only",
+			boolFlagName:        "only",
+			configFilePath:      wd + "/../../features/unit-test-configs/config-case-2.yml",
+			expectedErrorString: ErrConflictingOnlyFlag.Error(),
+		},
+	}
 
-	cmd.Flags().String("profile", "non-dev", "")
+	for _, tc := range testCases {
+		cmd, config := setupLoadInProfileParams(t, tc.configFilePath)
 
-	_, err = loadInProfileParams(cmd, *config, core.PackageSpec{})
-	if !assert.Equal(t, ErrConflictingDevFlag.Error(), err.Error()) {
-		t.FailNow()
+		setupBoolFlags(t, cmd, tc.boolFlagName)
+		cmd.Flags().String("profile", tc.profileName, "")
+
+		_, err = loadInProfileParams(cmd, *config, core.PackageSpec{})
+		if !assert.Equal(t, tc.expectedErrorString, err.Error()) {
+			t.FailNow()
+		}
 	}
 }
 
@@ -56,24 +100,8 @@ func setupLoadInProfileParams(t *testing.T, configFilePath string) (*cobra.Comma
 	return &cobra.Command{}, config
 }
 
-var configCaseOne = core.Config{
-	Image:    "jembi/go-cli-test-image",
-	LogPath:  "/tmp/logs",
-	Packages: []string{"client", "dashboard-visualiser-jsreport"},
-	CustomPackages: []core.CustomPackage{
-		{
-			Id:          "disi-on-platform",
-			Path:        "git@github.com:jembi/disi-on-platform.git",
-			SshKey:      "./id_rsa",
-			SshPassword: "./id_rsa_password.txt",
-		},
-	},
-	Profiles: []core.Profile{
-		{
-			Name:     "dev",
-			EnvFiles: []string{"./features/test-conf/.env.tests"},
-			Packages: []string{"dashboard-visualiser-jsreport", "disi-on-platform"},
-			Dev:      true,
-		},
-	},
+func setupBoolFlags(t *testing.T, cmd *cobra.Command, boolFlagName string) {
+	cmd.Flags().Bool(boolFlagName, false, "")
+	err := cmd.Flags().Set(boolFlagName, "true")
+	jtest.RequireNil(t, err)
 }
