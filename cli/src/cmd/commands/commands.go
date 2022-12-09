e49ab37a0847ff38cd0ae98b951aaafa544bb405
mark-labuschagne <marklabuschagne7@gmail.com>
2022-12-09 10:42:03 +0200

    add project skeleton command

--- cli/src/cmd/commands/commands.go
+++ cli/src/cmd/commands/commands.go
@@ -2,11 +2,15 @@ package commands
 
 import (
 	"cli/cmd/pkg"
+	"cli/cmd/project"
 
 	"github.com/spf13/cobra"
 )
 
 func AddCommands(cmd *cobra.Command) {
 	// TODO: add commands
-	cmd.AddCommand(pkg.DeclarePackageCommand())
+	cmd.AddCommand(
+		pkg.DeclarePackageCommand(),
+		project.ProjectGenerateCommand(),
+	)
 }
