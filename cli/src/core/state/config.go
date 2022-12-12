ce345c90171736f1542e94ed01f2183b691f1d6b
mark-labuschagne <marklabuschagne7@gmail.com>
2022-12-12 14:52:03 +0200

    set state for state.configViper

--- cli/src/core/state/config.go
+++ cli/src/core/state/config.go
@@ -1,6 +1,43 @@
 package state
 
-var (
-	ConfigFile string
-	EnvFiles   []string
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
 )
+
+var (
+	ConfigFile  string
+	EnvFiles    []string
+	configViper *viper.Viper
+)
+
+func SetConfigViper(configFile string) (*viper.Viper, error) {
+	configViper = viper.New()
+	if configFile != "" {
+		absFilePath, err := filepath.Abs(configFile)
+		if err != nil {
+			return nil, errors.Wrap(err, "")
+		}
+
+		configViper.SetConfigFile(absFilePath)
+	} else {
+		wd, err := os.Getwd()
+		if err != nil {
+			return nil, errors.Wrap(err, "")
+		}
+
+		configViper.AddConfigPath(wd)
+		configViper.SetConfigType("yaml")
+		configViper.SetConfigName("config")
+	}
+
+	err := configViper.ReadInConfig()
+	if err != nil {
+		return nil, errors.Wrap(err, "")
+	}
+
+	return configViper, nil
+}
