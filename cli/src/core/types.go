389752048fff5ff00eaad9f709cce02c233ec7e9
mark-labuschagne <marklabuschagne7@gmail.com>
2022-12-12 14:48:34 +0200

    declare types

---
+++ cli/src/core/types.go
@@ -0,0 +1,35 @@
+package core
+
+type Profile struct {
+	Name     string   `yaml:"name"`
+	EnvFiles []string `yaml:"envFiles"`
+	Dev      bool     `yaml:"dev"`
+	Only     bool     `yaml:"only"`
+	Packages []string `yaml:"packages"`
+}
+
+type CustomPackage struct {
+	Id   string `yaml:"id"`
+	Path string `yaml:"path"`
+}
+
+type Config struct {
+	Image          string          `yaml:"image"`
+	LogPath        string          `yaml:"logPath"`
+	Packages       []string        `yaml:"packages"`
+	CustomPackages []CustomPackage `yaml:"customPackages"`
+	Profiles       []Profile       `yaml:"profiles"`
+	ProjectName    string          `yaml:"projectName"`
+	PlatformImage  string          `yaml:"platformImage"`
+}
+
+type PackageSpec struct {
+	EnvironmentVariables []string
+	DeployCommand        string
+	Packages             []string
+	IsDev                bool
+	IsOnly               bool
+	CustomPackages       []CustomPackage
+	ImageVersion         string
+	TargetLauncher       string
+}
