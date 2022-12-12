0f23ab8389c5b0fb36b336be5f0c77cfcbd36707
mark-labuschagne <marklabuschagne7@gmail.com>
2022-12-12 14:37:29 +0200

    renamed config package to state

--- cli/src/cmd/root.go
+++ cli/src/cmd/root.go
@@ -7,7 +7,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"cli/cmd/commands"
-	"cli/core/config"
+	"cli/core/state"
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -30,9 +30,9 @@ func init() {
 	// TODO: read the docs for cobra.OnInitialize() and decide if it's needed
 	cobra.OnInitialize()
 
-	rootCmd.PersistentFlags().StringVar(&config.ConfigFile, "config", "", "config file (default is $WORKING_DIR/config.yaml)")
+	rootCmd.PersistentFlags().StringVar(&state.ConfigFile, "config", "", "config file (default is $WORKING_DIR/config.yaml)")
 	// Note: No shorthand for env-file, saving -e for individual env var declarations
-	rootCmd.PersistentFlags().StringSliceVar(&config.EnvFiles, "env-file", nil, "env file")
+	rootCmd.PersistentFlags().StringSliceVar(&state.EnvFiles, "env-file", nil, "env file")
 
 	commands.AddCommands(rootCmd)
 }
--- cli/src/core/state/config.go
+++ cli/src/core/state/config.go
@@ -1,4 +1,4 @@
-package config
+package state
 
 var (
 	ConfigFile string
