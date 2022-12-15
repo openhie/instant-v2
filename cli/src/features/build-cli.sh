c9c15e3dc1c38127b3da297d6e2d06867b7117fa
mark-labuschagne <marklabuschagne7@gmail.com>
2022-12-15 14:56:00 +0200

    godog build-cli.sh file disables CGO this is how the CLI is intended to be used, so the tests should build it the same way.

--- cli/src/features/build-cli.sh
+++ cli/src/features/build-cli.sh
@@ -5,9 +5,9 @@ cp ./features/test-conf/* ../../cli-tmp/src
 
 cd ../../cli-tmp/src || exit
 
-GOOS=darwin GOARCH=amd64 go build -o ../../cli/src/features/test-platform-macos
-GOOS=linux GOARCH=amd64 go build -o ../../cli/src/features/test-platform-linux
-GOOS=windows GOARCH=amd64 go build -o ../../cli/src/features/test-platform.exe
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ../../cli/src/features/test-platform-macos
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ../../cli/src/features/test-platform-linux
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ../../cli/src/features/test-platform.exe
 go clean
 
 cd ../.. || exit
