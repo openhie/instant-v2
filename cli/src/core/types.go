8233940e78275161d3cd102b514ce87a93a1c896
mark-labuschagne <marklabuschagne7@gmail.com>
2023-01-05 21:24:24 +0200

    reintroduced package generate with tests

--- cli/src/core/types.go
+++ cli/src/core/types.go
@@ -33,3 +33,15 @@ type PackageSpec struct {
 	ImageVersion         string
 	TargetLauncher       string
 }
+
+type GeneratePackageSpec struct {
+	Id             string
+	Name           string
+	Image          string
+	Stack          string
+	Description    string
+	Type           string
+	IncludeDevFile bool
+	TargetPort     string
+	PublishedPort  string
+}
